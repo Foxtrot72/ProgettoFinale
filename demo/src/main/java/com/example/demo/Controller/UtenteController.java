88697cbf9ef4562c42a1723df55b40ee160eb54c
Andra Stan <andrabeatricecorina.stan@gmail.com>
2024-06-13 17:29:25 +0200

    view login creato e connesso

--- demo/src/main/java/com/example/demo/Controller/UtenteController.java
+++ demo/src/main/java/com/example/demo/Controller/UtenteController.java
@@ -8,42 +8,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.example.demo.Model.Utente;
-import com.example.demo.Repository.ProdottoRepository;
 import com.example.demo.Repository.UtenteRepository;
-
-
 
 @Controller
 public class UtenteController {
-    
-    @Autowired
-    private ProdottoRepository prodottoRepository;
 
     @Autowired
     private UtenteRepository utenteRepository;
+
+    public Utente findByUsername(String username) {
+        return utenteRepository.findByUsername(username);
+    }
 
     @GetMapping("/")
     public String paginaIniziale() {
         return "Inizio";
     }
 
-    // @GetMapping("/utenti")
-    // public @ResponseBody void getAllUtenti() {
-    //   utenteRepository.findAll();
-    //   return;
-    // }
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("/utenti")
     public String getAllutenti(Model model) {
         List<Utente> utenti = utenteRepository.findAll();
         model.addAttribute("utenti", utenti);
-        return "Utenti"; 
+        return "Utenti";
     }
-    
 
     @GetMapping("/sign")
     public String mostraFormSign(Utente utente, Model model) {
@@ -52,34 +45,25 @@ public class UtenteController {
     }
 
     @PostMapping("/sign")
-    public  String aggiungiUtente(Utente utente) {
+    public String aggiungiUtente(Utente utente) {
         utenteRepository.save(utente);
-        // model.addAttribute("utente", utente);
-        return "redirect:/utenti"; 
-    } 
+        return "redirect:/home";
+    }
 
-//
+    @GetMapping("/login")
+    public String mostraFormLogin(Model model) {
+        model.addAttribute("utente", new Utente());
+        return "Login";
+    }
 
-
-//     @PostMapping("/sign")
-// public ModelAndView aggiungiUtente(Utente utente) {
-//     utenteRepository.save(utente);
-//     ModelAndView mav = new ModelAndView("redirect:/utenti");
-//     mav.addObject("messaggio", "Utente aggiunto con successo");
-//     return mav;
-// }
-
-
-    // @GetMapping("/login")
-    // public String mostraFormLogin(@RequestParam("id") Long id, Model model) {
-    //     Optional<Utente> optionalUtente = UtenteRepository.findById(id);
-    //     if (optionalUtente.isPresent()) {
-    //         model.addAttribute("utente", optionalUtente.get());
-    //         return "Login";
-    //     } else {
-    //         return "redirect:/home";
-    //     }
-    // }
-
+    @PostMapping("/login")
+    public String autentificaUtente(@ModelAttribute("utente") Utente utente, Model model) {
+        Utente existingUtente = findByUsername(utente.getUsername());
+        if (existingUtente != null && existingUtente.getPassword().equals(utente.getPassword())) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Username o password non corretti");
+            return "login";
+        }
+    }
 }
-
