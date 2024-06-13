f16874974fcb360463d2d3f157d8a236dd8f62df
Foxtrot72 <155225676+Foxtrot72@users.noreply.github.com>
2024-06-13 18:45:35 +0200

    Aggiunta login

--- demo/src/main/java/com/example/demo/Controller/UtenteController.java
+++ demo/src/main/java/com/example/demo/Controller/UtenteController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.Model.Prodotto;
 import com.example.demo.Model.Utente;
 import com.example.demo.Repository.ProdottoRepository;
 import com.example.demo.Repository.UtenteRepository;
@@ -27,7 +29,10 @@ public class UtenteController {
 
     @Autowired
     private UtenteRepository utenteRepository;
-
+    
+public Utente findByUsername(String username){
+    return utenteRepository.findByUsername(username);
+}
     @GetMapping("/")
     public String paginaIniziale() {
         return "Inizio";
@@ -75,7 +80,19 @@ public class UtenteController {
        
         return "Inizio";
     }
+    @GetMapping("/acquirente")
+    public String mostraFromProdotti(Prodotto prodotto, Model model) {
+        model.addAttribute("prodotto", prodotto);
+        return "Acquirente";
+    }
 
+
+    // @PostMapping("/acquirente")
+    // public String ricercaProdotto(String nome, Model model) {
+    //     List<Prodotto> prodotti = prodottoRepository.findByNome(nome);
+    //     model.addAttribute("prodotti", prodotti);
+    //     return "redirect:/";
+    // }
     
 //
 
@@ -89,16 +106,21 @@ public class UtenteController {
 // }
 
 
-    // @GetMapping("/login")
-    // public String mostraFormLogin(@RequestParam("id") Long id, Model model) {
-    //     Optional<Utente> optionalUtente = UtenteRepository.findById(id);
-    //     if (optionalUtente.isPresent()) {
-    //         model.addAttribute("utente", optionalUtente.get());
-    //         return "Login";
-    //     } else {
-    //         return "redirect:/home";
-    //     }
-    // }
+@GetMapping("/login")
+public String mostraFormLogin(Model model) {
+    model.addAttribute("utente", new Utente());
+    return "Login";
+}
 
+@PostMapping("/login")
+public String autentificaUtente(@ModelAttribute("utente") Utente utente, Model model) {
+    Utente existingUtente = findByUsername(utente.getUsername());
+    if (existingUtente != null && existingUtente.getPassword().equals(utente.getPassword())) {
+        return "redirect:/home";
+    } else {
+        model.addAttribute("error", "Username o password non corretti");
+        return "login";
+    }
+}
 }
 
