0460b6b34258aafb33540672994492cdd636d92e
Foxtrot72 <155225676+Foxtrot72@users.noreply.github.com>
2024-06-13 21:02:48 +0200

    Funziona ora con il catalogo

--- demo/src/main/java/com/example/demo/Controller/UtenteController.java
+++ demo/src/main/java/com/example/demo/Controller/UtenteController.java
@@ -1,7 +1,6 @@
 package com.example.demo.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,14 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 import com.example.demo.Model.Prodotto;
 import com.example.demo.Model.Utente;
 import com.example.demo.Repository.UtenteRepository;
-
-
-
-
 
 @Controller
 public class UtenteController {
@@ -28,7 +22,6 @@ public class UtenteController {
     public Utente findByUsername(String username) {
         return utenteRepository.findByUsername(username);
     }
-    
 
     @GetMapping("/")
     public String paginaIniziale() {
@@ -57,19 +50,25 @@ public class UtenteController {
     public String aggiungiUtente(Utente utente) {
         utenteRepository.save(utente);
         // model.addAttribute("utente", utente);
-        return "redirect:/home"; 
-    } 
+        return "redirect:/home";
+    }
 
-   
     @GetMapping("/venditore")
     public String mostraVenditore() {
-       
+
         return "venditore";
     }
+
     @GetMapping("/inizio")
     public String mostraInzio() {
-       
+
         return "Inizio";
+    }
+
+    @GetMapping("/prodotticatalogo")
+    public String mostraCatalogo() {
+
+        return "Catalogo";
     }
     @GetMapping("/acquirente")
     public String mostraFromProdotti(Prodotto prodotto, Model model) {
@@ -77,41 +76,40 @@ public class UtenteController {
         return "Acquirente";
     }
 
-
     // @PostMapping("/acquirente")
     // public String ricercaProdotto(String nome, Model model) {
-    //     List<Prodotto> prodotti = prodottoRepository.findByNome(nome);
-    //     model.addAttribute("prodotti", prodotti);
-    //     return "redirect:/";
+    // List<Prodotto> prodotti = prodottoRepository.findByNome(nome);
+    // model.addAttribute("prodotti", prodotti);
+    // return "redirect:/";
     // }
-    
-//
 
+    //
 
-//     @PostMapping("/sign")
-// public ModelAndView aggiungiUtente(Utente utente) {
-//     utenteRepository.save(utente);
-//     ModelAndView mav = new ModelAndView("redirect:/utenti");
-//     mav.addObject("messaggio", "Utente aggiunto con successo");
-//     return mav;
-// }
+    // @PostMapping("/sign")
+    // public ModelAndView aggiungiUtente(Utente utente) {
+    // utenteRepository.save(utente);
+    // ModelAndView mav = new ModelAndView("redirect:/utenti");
+    // mav.addObject("messaggio", "Utente aggiunto con successo");
+    // return mav;
+    // }
 
-
-@GetMapping("/login")
-public String mostraFormLogin(Model model) {
-    model.addAttribute("utente", new Utente());
-    return "Login";
-}
-
-@PostMapping("/login")
-public String autentificaUtente(@ModelAttribute("utente") Utente utente, Model model) {
-    Utente existingUtente = findByUsername(utente.getUsername());
-    if (existingUtente != null && existingUtente.getPassword().equals(utente.getPassword())) {
-        return "redirect:/home";
-    } else {
-        model.addAttribute("error", "Username o password non corretti");
+    @GetMapping("/login")
+    public String mostraFormLogin(Model model) {
+        model.addAttribute("utente", new Utente());
         return "Login";
     }
-}
-}
 
+    @PostMapping("/login")
+    public String autentificaUtente(@ModelAttribute("utente") Utente utente, Model model) {
+        Utente existingUtente = findByUsername(utente.getUsername());
+        if (existingUtente != null && existingUtente.getPassword().equals(utente.getPassword())) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Username o password non corretti");
+            return "Login";
+        }
+    
+    
+
+    }
+}
